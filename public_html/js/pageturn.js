2d775d83d422edd68492ed180df72b1490816f6b
marcor75 <marcor75@Renhao>
2014-11-28 14:01:39 +0800

    revised page turning function in js and z-index stacking order in css and finally solved issue w/ non-clickable images in firefox!!

--- public_html/js/pageturn.js
+++ public_html/js/pageturn.js
@@ -24,16 +24,14 @@ var navArrowsFirstLast = function() {
     }; 
 };
 
-//on PAGE CLICK-navigation. Is it possible to disable for firefox?? (updt.:seems to work now)
-//$(".page").click(function () {  -->changed to delegate method below
+//on PAGE CLICK-navigation. Not reliable in firefox; workaround w/ larger nav arrows 
 $(".page").delegate("div", "click", function(){    
     // check if page has been turned over
     if ($(this).parent(".page").hasClass("turned")) {
         // turn page back to original position
         turnLeftPage();
-        navArrowsFirstLast ();
-
-        // else page IS in original position on right 
+       
+    // else page IS in original position on right 
     } else {
         // turn over to the left
         turnRightPage();
@@ -43,24 +41,24 @@ $(".page").delegate("div", "click", function(){
 // turns over right page    
 function turnRightPage(callback) {
     var $page = $(".turned").prev(".page:not(.last)");
-    $page.prev(".page").addClass("open"); //new
+    $page.prev(".page").addClass("open"); //class open makes images on that page visible
     $page.css({
         "transform": "rotateY(-180deg)",
         "transform-origin": "left"
     });
 
-    var turnedZindex = parseInt($(".turned").css("z-index"));
-    // time visibility of top page in line w. transition time     
-    $page.css("z-index", turnedZindex + 1);
+    var currentZindex = parseInt($page.css("z-index"));
+    // set new z-index; 100 is arbitraryly chosen number to make index change easily reversible - not very 'future proof' though (e.g. if more pages added)    
+    $page.css("z-index", 100 - currentZindex);
 
     //change class AFTER transition (frm. treehouse-site)
     $page.on('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function () {
         $page.unbind('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
         
         //need to double-set z-index or else second time turning page open setting z-index does not work (tried in Chrome 38.0.2125.111 m)
-        $page.css("z-index", turnedZindex + 1);
+        $page.css("z-index", 100 - currentZindex);
         $(".turned").removeClass("turned");
-        $page.removeClass("open"); //new
+        $page.removeClass("open"); 
         $page.addClass("turned");
         
         navArrowsFirstLast ();
@@ -78,7 +76,7 @@ function turnLeftPage(callback) {
     var $prevPage = $page.next();
     $prevPage.addClass("turned");
     
-    $page.addClass("open");                 //new
+    $page.addClass("open");                 
     $page.css({
         "transform": "rotateY(0deg)",
         "transform-origin": "left"
@@ -89,11 +87,11 @@ function turnLeftPage(callback) {
     // after turning change class and z-index        
     $page.on('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function () {
         $page.unbind('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
-        $page.prev(".page").removeClass("open"); //new
+        $page.prev(".page").removeClass("open"); 
         $page.removeClass("turned");
         
-        
-        $page.css("z-index", currentZindex - (currentZindex-1));
+        // set new z-index; 100 is arbitraryly chosen number to make index change easily reversible - not very 'future proof' though (e.g. if more pages added)    
+        $page.css("z-index", 100 - currentZindex);
         
         navArrowsFirstLast ();
         //callback function thx to Antoine Combes
